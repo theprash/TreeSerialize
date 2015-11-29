79b8641b574133bdaa22c5e90fb9a00c3bc0ca06
Prashant Pathak <theprash@gmail.com>
2015-11-29 18:52:38 +0000

    Build the tree of serialized size data.

--- TreeSerialize/TreeSerialize.fs
+++ TreeSerialize/TreeSerialize.fs
@@ -11,17 +11,40 @@ module Serialize =
         formatter.Serialize(stream, obj)
         stream.Length
 
+    let name obj = obj.GetType().FullName
+
     let children obj =
-        obj.GetType().GetFields(BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Instance)
-        |> Seq.filter (fun field -> not field.IsNotSerialized)
-        |> Seq.choose (fun field ->
-            match field.GetValue(obj) with
-            | null -> None
-            | value -> Some value)
+        let objType = obj.GetType()
+
+        if objType.FullName.StartsWith "System." then
+            Seq.empty
+
+        else
+            objType
+               .GetFields(BindingFlags.Instance ||| BindingFlags.NonPublic ||| BindingFlags.Instance)
+            |> Seq.filter (fun field -> not field.IsNotSerialized)
+            |> Seq.choose (fun field ->
+                match field.GetValue(obj) with
+                | null -> None
+                | value -> Some value)
 
 module Chart =
-    let makeTree getSize getChildren obj =
-        obj |> getChildren |> Seq.map getSize
+    type NodeData = {Label : string; Size : int64}
+    type Branch = {Data : NodeData; Children : seq<Tree>}
+    and Tree = Branch of Branch | Leaf of NodeData
+
+    let rec makeTree getSize getLabel getChildren obj =
+        let label = getLabel obj
+
+        printfn "%A" label
+
+        match getChildren obj |> Seq.toList with
+        | [] ->
+            Leaf {Label = label; Size = getSize obj}
+        | childObjs ->
+            let children = childObjs |> Seq.map (makeTree getSize getLabel getChildren)
+            let sum = children |> Seq.map (function Leaf l -> l.Size | Branch b -> b.Data.Size) |> Seq.sum
+            Branch {Data = {Label = label; Size = sum}; Children = children}
 
 type TreeSerializer =
-    static member MakeTree obj = Chart.makeTree Serialize.serializedLength Serialize.children obj
+    static member MakeTree obj = Chart.makeTree Serialize.serializedLength Serialize.name Serialize.children obj
